c1e6eaaf7606227e0800af0e85175aa686b40c11
redcliphaloe <redcliphaloe@gmail.com>
2020-03-16 22:55:04 +0300

    refactor: delete trash

--- src/index.js
+++ src/index.js
@@ -29,110 +29,21 @@ module.exports = function toReadable (number) {
         90: 'ninety'
     }
     let result = '';
-    let subNumber;
-    if (number > 1000000000) {
-        subNumber = Math.floor(number / 1000000000);
-        if (subNumber >= 100) {
-            result += textValues[('' + subNumber)[0]] + ' hundred';
-            if (subNumber >= 200) {
-                result += 's';
-            }
-            result += ' ';
-            subNumber = subNumber % 100;
-        }    
-        if (subNumber >= 20) {
-            result += textValues[subNumber - subNumber % 10];
-            if (subNumber % 10) {
-                result += ' ' + textValues[subNumber % 10];
-            }
-            result += ' billions'; 
-        } else {
-            if (subNumber >= 10) {
-                result += textValues[subNumber] + ' billions';
-            } else {
-                result += textValues[subNumber] + ' billion';
-            }
-        }
-        result += ' ';
-        number = number - Math.floor(number / 1000000000) * 1000000000;
-    }
-
-    if (number > 1000000) {
-        subNumber = Math.floor(number / 1000000);
-        if (subNumber >= 100) {
-            result += textValues[('' + subNumber)[0]] + ' hundred';
-            if (subNumber >= 200) {
-                result += 's';
-            }
-            result += ' ';
-            subNumber = subNumber % 100;
-        }       
-        if (subNumber >= 20) {
-            result += textValues[subNumber - subNumber % 10];
-            if ((subNumber % 10)) {
-                result += ' ' + textValues[subNumber % 10];
-            }
-            result += ' millions'; 
-        } else {
-            if (subNumber >= 10) {
-                result += textValues[subNumber] + ' millions';
-            } else {
-                result += textValues[subNumber] + ' million';
-            }
-        }
-        result += ' ';    
-        number = number - Math.floor(number / 1000000) * 1000000;
-    }
-
-    if (number > 1000) {
-        subNumber = Math.floor(number / 1000);
-        if (subNumber >= 100) {
-            result += textValues[('' + subNumber)[0]] + ' hundred';
-            if (subNumber >= 200) {
-                result += 's';
-            }
-            result += ' ';
-            subNumber = subNumber % 100;
-        }       
-        if (subNumber >= 20) {
-            result += textValues[subNumber - subNumber % 10];
-            if ((subNumber % 10)) {
-                result += ' ' + textValues[subNumber % 10];
-            }
-            result += ' thousands'; 
-        } else {
-            if (subNumber >= 10) {
-                result += textValues[subNumber] + ' thousands';
-            } else {
-                result += textValues[subNumber] + ' thousand';
-            }
-        }
-        result += ' ';  
-        number = number - Math.floor(number / 1000) * 1000; 
-    }
-
-    subNumber = number;
-    if (subNumber >= 100) {
-        result += textValues[('' + subNumber)[0]] + ' hundred';
-        // if (subNumber >= 200) {
-        //     result += 's';
-        // }
-        result += ' ';
-        subNumber = subNumber % 100;
+    if (number >= 100) {
+        result += textValues[('' + number)[0]] + ' hundred ';
+        number = number % 100;
     }       
-    if (subNumber >= 20) {
-        result += textValues[subNumber - subNumber % 10];
-        if ((subNumber % 10)) {
-            result += ' ' + textValues[subNumber % 10];
+    if (number >= 20) {
+        result += textValues[number - number % 10];
+        if ((number % 10)) {
+            result += ' ' + textValues[number % 10];
         }
     } else {
-        if (subNumber >= 10) {
-            result += textValues[subNumber];
-        } else if (subNumber > 0) {
-            result += textValues[subNumber];
+        if (number >= 10) {
+            result += textValues[number];
+        } else if (number > 0) {
+            result += textValues[number];
         }
     }    
     return result.trim() || 'zero';
 }
-
-// console.log(toReadable(11012015001));
